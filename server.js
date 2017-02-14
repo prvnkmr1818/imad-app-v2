b5dd0777e7f3fa27a7226ca93c272b1a7f70b1b0
prvnkmr1818 <prvnkmr1818@gmail.com>
2017-02-14 17:16:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one',function(req,res){
+    res.send("Article one will be served here");
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
